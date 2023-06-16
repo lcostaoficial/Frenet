cde17c50dc1e412f430ee48917bd3e6592c11767
Lucas <Lucas@NOTEBOOK>
2023-06-16 10:52:24 -0400

    Migração de layout para o AdminLTE e implementação dos recursos para alteração de View em tempo de execução.

---
+++ FrenetApi/Controllers/ShippingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrenetApi.Controllers
+{
+    public class ShippingController : Controller
+    {
+        public IActionResult Quote()
+        {
+            return View();
+        }
+    }
+}
--- FrenetApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FrenetApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
