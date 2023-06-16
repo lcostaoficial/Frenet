62551cf888f1715e2591e3d5d5c56c24bb3c4b31
Lucas <Lucas@NOTEBOOK>
2023-06-15 23:37:24 -0400

    Projeto inicial a partir do modelo padrão do asp.net mvc

---
+++ FrenetApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FrenetApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
