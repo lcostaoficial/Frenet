24e4ed2ec67c9923fc327a9fd0d4a1581b22a955
Lucas <Lucas@NOTEBOOK>
2023-06-16 10:11:19 -0400

    Preparação das ViewsModels

---
+++ FrenetApi/Models/ViewModel/ShippingItemArrayVm.cs
@@ -0,0 +1,13 @@
+﻿namespace FrenetApi.Models.ViewModel
+{
+    public class ShippingItemArrayVm
+    {
+        public int? Height { get; set; }
+        public int? Length { get; set; }
+        public int? Quantity { get; set; }
+        public double? Weight { get; set; }
+        public int? Width { get; set; }
+        public string? SKU { get; set; }
+        public string? Category { get; set; }
+    }
+}
---
+++ FrenetApi/Models/ViewModel/ShippingQuoteVm.cs
@@ -0,0 +1,12 @@
+﻿namespace FrenetApi.Models.ViewModel
+{
+    public class ShippingQuoteVm
+    {
+        public string? SellerCEP { get; set; }
+        public string? RecipientCEP { get; set; }
+        public double? ShipmentInvoiceValue { get; set; }
+        public string? ShippingServiceCode { get; set; }
+        public List<ShippingItemArrayVm>? ShippingItemArray { get; set; }
+        public string? RecipientCountry { get; set; }
+    }
+}
