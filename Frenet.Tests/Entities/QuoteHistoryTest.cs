0978c6d335c9d729ccf0ebefe8cfefa94d38f30b
Lucas <Lucas@NOTEBOOK>
2023-06-19 03:24:47 -0400

    Inclusão de teste unitário básico

---
+++ Frenet.Tests/Entities/QuoteHistoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Frenet.Application.Models.Entities;
+
+namespace Frenet.Tests.Entities
+{
+    public class QuoteHistoryTest
+    {
+        [Fact]
+        public void GerarDataDeCriacao_SetsCreationDateToCurrentDateTime()
+        {
+            // Arrange
+            var quoteHistory = new QuoteHistory();
+
+            // Act
+            quoteHistory.GerarDataDeCriacao();
+
+            // Assert
+            Assert.Equal(DateTime.Now, quoteHistory.CreationDate, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public void InserirSellerCEP_SetsSellerCEP()
+        {
+            // Arrange
+            var quoteHistory = new QuoteHistory();
+            var sellerCep = "12345678";
+
+            // Act
+            quoteHistory.InserirSellerCEP(sellerCep);
+
+            // Assert
+            Assert.Equal(sellerCep, quoteHistory.SellerCEP);
+        }
+    }
+}
